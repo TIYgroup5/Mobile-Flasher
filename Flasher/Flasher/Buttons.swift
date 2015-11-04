0e9490d70d7d694f6a24916d42665d7af25ceff9
Anjel0552 <anjelvillafranco0552@gmail.com>
2015-11-04 16:14:26 -0500

    init

---
+++ Flasher/Flasher/Buttons.swift
@@ -0,0 +1,32 @@
+//
+//  Buttons.swift
+//  Flasher
+//
+//  Created by Anjel Villafranco on 11/4/15.
+//  Copyright © 2015 Anjel Villafranco. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class TheButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0
+    
+    override func drawRect(rect: CGRect) {
+        
+        
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = cornerRadius > 0
+        layer.borderWidth = borderWidth
+//        layer.borderColor = UI
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0
+    
+    
+}
+
+
+
